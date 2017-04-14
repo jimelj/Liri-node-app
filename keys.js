f3dfb4eba2553ce9bf4f54b72ea136d5786a0029
Jimel J. Joseph <jimelj@gmail.com>
2017-04-14 09:17:48 -0400

    setting up repo and downloading npm packages

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'qLLv6U7GNNZqgBsxEF5TDuJ8I',
+  consumer_secret: 'EyrCC3BxLy0zMuKKpIHMsCmA2cxGZTYoUI6vmbAt2EE8pyuPpC',
+  access_token_key: '	64686710-bQRvqaFUOJgdTjDFbPoeVKFNTwhJRyJCPTHJrHOsA',
+  access_token_secret: 'gGtxA2XvbWCj6WOrJH7YPeYsSxuZmCaY2rUAhnKTSjyse',
+};
